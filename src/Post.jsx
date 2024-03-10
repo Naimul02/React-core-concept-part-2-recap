e0f9bd4cb6a4fe6b5ad0b1e363aacf72e5c8e9b5
Naimul02 <naimulislum39@gmail.com>
2024-03-10 11:10:46 +0600

    commit no 1

---
+++ src/Post.jsx
@@ -0,0 +1,12 @@
+import './Post.css'
+export default function Post({post}){
+  const {id , title , userId , body } = post
+    return(
+        <div className='post'>
+            <h5>Title : {title}</h5>
+            <p><small>userId : {userId} </small></p>
+            <p><small>postId :  {id}</small></p>
+            <p>{body}</p>
+        </div>
+    )
+}
